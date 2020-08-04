a3dd40fa288d5fa5d3876508530f67f8e8a9ee66
Harald Gronlie Nyhus <gronyh@icloud.com>
2020-08-04 12:45:05 +0200

    add spek2 and assertk dependencies

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    testCompileOnly("com.willowtreeapps.assertk:assertk-jvm:0.22")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.9")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.9")
+    testRuntimeOnly(kotlin("reflect"))
 }
 
 tasks {
@@ -19,5 +24,10 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
     }
 }
